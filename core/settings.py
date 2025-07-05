0670e4993fbd9b2534fe9bb7bbebe3b9b7db2bbd
nazeer <tahaira8244@gmail.com>
2025-07-05 17:41:42 +0500

    change the database

--- core/settings.py
+++ core/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '9444',
+        'PORT':'5432',
     }
 }
 
